fc677fb512f6bd4e63e0b353c29c749dc0cf1d0f
Kerdo <kerdokurs@gmail.com>
2019-02-10 20:45:50 +0200

    Esimest korda külastajale info näitamine.

--- core/routers/app.js
+++ core/routers/app.js
@@ -10,39 +10,43 @@ const Bookmark = require('../database/models/Bookmark');
 const User = require('../database/models/User');
 
 router.route('/').get(async (req, res) => {
-  if (await authModule.isUserLoggedIn(req)) {
-    const session = await authModule.getSession(req);
-    const { uid } = session;
-
-    const recentBookmarks = await Bookmark.find({ uid })
-      .sort({ timestamp: 'desc' })
-      .limit(5)
-      .then(data => data)
-      .catch(err => functions.handle(err, '/core/routers/app.js'));
-
-    const user = await User.find({ uid })
-      .then(data => data[0])
-      .catch(err => functions.handle(err, '/core/routers/app.js'));
-
-    const { achievements } = user;
-    let recentAchievements = _.take(
-      _.orderBy(achievements, 'timestamp', 'desc'),
-      5
-    );
-    recentAchievements = recentAchievements.map(ach => {
-      ach.timestamp = functions.parseDate(ach.timestamp);
-      return ach;
-    });
-
-    res.render('index', {
-      recentBookmarks,
-      recentAchievements
-    });
+  if (!req.cookies['visited']) {
+    res.cookie('visited', 'true').redirect('/info');
   } else {
-    res.render('index', {
-      recentBookmarks: [],
-      recentAchievements: []
-    });
+    if (await authModule.isUserLoggedIn(req)) {
+      const session = await authModule.getSession(req);
+      const { uid } = session;
+
+      const recentBookmarks = await Bookmark.find({ uid })
+        .sort({ timestamp: 'desc' })
+        .limit(5)
+        .then(data => data)
+        .catch(err => functions.handle(err, '/core/routers/app.js'));
+
+      const user = await User.find({ uid })
+        .then(data => data[0])
+        .catch(err => functions.handle(err, '/core/routers/app.js'));
+
+      const { achievements } = user;
+      let recentAchievements = _.take(
+        _.orderBy(achievements, 'timestamp', 'desc'),
+        5
+      );
+      recentAchievements = recentAchievements.map(ach => {
+        ach.timestamp = functions.parseDate(ach.timestamp);
+        return ach;
+      });
+
+      res.render('index', {
+        recentBookmarks,
+        recentAchievements
+      });
+    } else {
+      res.render('index', {
+        recentBookmarks: [],
+        recentAchievements: []
+      });
+    }
   }
 });
 
